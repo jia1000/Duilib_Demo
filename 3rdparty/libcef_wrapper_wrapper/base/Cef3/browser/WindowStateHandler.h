4f9fff14f82528c6bcbfc3433df89048af0f97a2
jiaYF <jiajiajiajia10003@163.com>
2019-04-15 15:40:59 +0800

    Duilib_Demo版本：增加对libcef_dll_wrapper中浏览器控件的封装。

---
+++ 3rdparty/libcef_wrapper_wrapper/base/Cef3/browser/WindowStateHandler.h
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <string>
+
+#include "include/wrapper/cef_message_router.h"
+#include "include/wrapper/cef_stream_resource_handler.h"
+
+const char kMessageName[] = "WindowState";
+
+// define window state.
+#define WINDOW_MAXIMIZED "MAXIMIZED"
+#define WINDOW_MINIMIZED "MINIMIZED"
+#define WINDOW_NORMAL "NORMAL"
+#define WINDOW_UNKNOWN_MSG "Unknown State"
+
+// Handle messages in the browser process.
+class WindowStateHandler : public CefMessageRouterBrowserSide::Handler 
+{
+public:
+	WindowStateHandler() {}
+
+	virtual bool OnQuery(CefRefPtr<CefBrowser> browser,
+		CefRefPtr<CefFrame> frame,
+		int64 query_id,
+		const CefString& request,
+		bool persistent,
+		CefRefPtr<Callback> callback) OVERRIDE 
+	{
+		const std::string& message_name = request;
+ 		if (message_name.find(kMessageName) == 0) {
+			// 拿到当前窗体
+			
+			return true;
+ 		}
+		// let other handlers to process.
+		return false;
+	}
+
+	virtual void OnQueryCanceled(CefRefPtr<CefBrowser> browser,
+		CefRefPtr<CefFrame> frame,
+		int64 query_id) OVERRIDE
+	{
+		//LOG(INFO) << "Query request [Id: " << query_id << "] was cancelled.";
+
+		// cancel async query task...
+	}
+};
---
+++ 3rdparty/libcef_wrapper_wrapper/base/Cef3/renderer/client_v8handler.cpp
@@ -0,0 +1,71 @@
+#include "StdAfx.h"
+#include "client_v8handler.h"
+
+//#define USE_AES
+
+bool V8Handler::Execute( const CefString& name, CefRefPtr<CefV8Value> object, \
+							const CefV8ValueList& arguments, CefRefPtr<CefV8Value>& retval, CefString& exception )
+{
+	CefRefPtr<CefV8Context> context = CefV8Context::GetCurrentContext();
+
+	CefRefPtr<CefProcessMessage> msg = CefProcessMessage::Create( name );
+
+	// Retrieve the argument list object.
+	CefRefPtr<CefListValue> args = msg->GetArgumentList();
+
+	// Populate the argument values.
+	int _size = arguments.size();
+
+	args->SetSize(_size);
+
+	for (int i = 0; i < _size; i++)
+	{
+		if (arguments[i]->IsString())
+		{
+			args->SetString(i, arguments[i]->GetStringValue());
+		}
+		else if (arguments[i]->IsInt())
+		{
+			args->SetInt(i, arguments[i]->GetIntValue());
+		}
+		else if (arguments[i]->IsDouble())
+		{
+			args->SetDouble(i, arguments[i]->GetDoubleValue());
+		}
+		else if (arguments[i]->IsBool())
+		{
+			args->SetBool(i, arguments[i]->GetBoolValue());
+		}
+		else
+		{
+			args->SetString(i, arguments[i]->GetStringValue());
+		}
+	}
+
+	//handle custom functions for js call
+	if (msg->GetName() == V8WindowFunciotn::loginFunc){
+		_size = arguments.size();
+
+		std::string d1 = args->GetString(0);
+		std::string d2 = args->GetString(1);
+
+		retval = CefV8Value::CreateString(d1 + " " + d2);
+
+	}else if(msg->GetName() == V8WindowFunciotn::domTest){
+
+		/*_size = arguments.size();		
+		CefRefPtr<CefDOMVisitor> visitor = new SimpleDomVisitor();
+		context->GetBrowser()->GetMainFrame()->VisitDOM(visitor);*/
+	}
+
+	context->GetBrowser()->SendProcessMessage(PID_BROWSER, msg);
+
+	return true;
+}
+
+std::string V8Handler::GenerateKey(const string& str)
+{
+	return str;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
