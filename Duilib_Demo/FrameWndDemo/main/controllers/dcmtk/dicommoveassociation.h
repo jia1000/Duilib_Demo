9a2862b43bc8186be72e6693afe742239527f80d
jiaYF <jiajiajiajia10003@163.com>
2019-08-30 17:25:19 +0800

    Duilib_Demo版本：PACS分支：添加了moveassociation类，但是，没有添加它的调用

---
+++ Duilib_Demo/FrameWndDemo/main/controllers/dcmtk/dicommoveassociation.h
@@ -0,0 +1,46 @@
+/*
+*  
+*  $Id: dicommoveassociation.h $
+*  DeepWise Dicom Viewer
+*
+*  Copyright 2017-2030 DeepWise All rights reserved.
+*  http://www.deepwise.com
+*
+*  This file is licensed under LGPL v3 license.
+*  See License.txt for details
+*
+*  Code adapted from Aeskulap
+*
+*/
+#pragma once
+#include <ctime>
+
+#include "dicomfindassociation.h"
+//#include "istorecallback.h"
+
+
+class MoveAssociation : public FindAssociation {
+public:
+
+	MoveAssociation(const std::string& ambitolog);//, IModeloDicom* pModelo=NULL);
+	virtual ~MoveAssociation();
+
+	//void SetModelo(IModeloDicom* pModelo);
+
+	void Create(const std::string& title, const std::string& peer, int port, const std::string& ouraet, /*int ourPort,*/ const char *abstractSyntax = UID_MOVEStudyRootQueryRetrieveInformationModel);
+
+	CONDITION SendObject(DcmDataset *dataset);
+
+
+protected:
+
+	void OnAddPresentationContext(T_ASC_Parameters *params);
+
+private:
+
+	CONDITION moveSCU(DcmDataset *pdset);
+
+	int m_maxReceivePDULength;
+	//IModeloDicom* m_pModelo;
+	std::string m_errorMessage;
+};
